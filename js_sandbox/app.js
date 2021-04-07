1a6e92078d1dd5bbe0cd84399181450a1f974a9f
Leanne Lacey-Byrne <llacey-byrne@saadian.com>
2021-04-07 08:52:33 +0100

    console.logs tables etc

---
+++ js_sandbox/app.js
@@ -0,0 +1,12 @@
+// log
+console.log('Hello');
+console.log(123);
+console.log(true);
+console.log('Hello');
+console.log([1,2,3,4]);
+console.time('Hello');
+console.log({a:1, b:2});
+console.error('This is some error');
+//console.clear();
+console.warn('This is a warning');
+console.timeEnd('Hello');
